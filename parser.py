3159473bfbbbad005cb47f07864ec43075d2a606
afsheenkhan-24 <afsheenkhan11@yahoo.com>
2026-02-06 17:11:10 +0000

    parser created to read file

--- parser.py
+++ parser.py
@@ -1,1 +1,9 @@
-file = open("test.py", "r")
+def read_file(file_path):
+    try:
+        with open(file_path, "r") as file:
+            content = file.read()
+            return content
+    except FileNotFoundError:
+        print(f"Error: The file '{file_path}' was not found.")
+    except IOError:
+        print(f"Error: An I/O error occurred while reading the file '{file_path}'.")
